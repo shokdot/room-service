36ce63daef54a12caa73f05696d90c7aeeee59d1
Henry Aleksanian (Shok.) <henula09106@gmail.com>
2026-01-18 23:00:57 +0400

    feat: update package.json and tsup.config.ts with new dependencies

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,25 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-	entry: ['src/server.ts'],
-	format: ['esm'],
-	target: 'es2022',
-	outDir: 'dist',
-	external: ['prom-client', 'fastify-metrics', 'axios', 'form-data', 'combined-stream'],
+    entry: ['src/server.ts'],
+    format: ['esm'],
+    target: 'es2022',
+    outDir: 'dist',
+    external: [
+        'fastify-metrics',
+        'pino',
+        'fastify',
+        '@fastify/cookie',
+        '@fastify/cors',
+        '@fastify/helmet',
+        '@fastify/rate-limit',
+        '@fastify/swagger',
+        '@fastify/swagger-ui',
+        '@prisma/client',
+        'axios',
+        'dotenv',
+        'jsonwebtoken',
+        'zod'
+    ],
+    noExternal: ['@core'],
 });
